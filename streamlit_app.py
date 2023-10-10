8484eb4706b98193a0d3334fda5e651af8bcfeea
yvonneeeelim <141410050+yvonneeeelim@users.noreply.github.com>
2023-10-10 14:14:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,38 +14,10 @@ image = Image.open('snacks.jpg')
 #displaying the image on streamlit app
 st.image(image, use_column_width=True)
 
-# Custom CSS to style the title and subheader
-st.markdown(
-    """
-    <style>
-    .title {
-        font-size: 46px;
-        font-weight: bold;
-        color: #4CAF50;
-        text-align: center;
-        margin-bottom: 10px;
-    }
-    .subheader {
-        font-size: 18px;
-        font-style: italic;
-        color: #777;
-        text-align: center;
-        margin-bottom: 20px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
-# Title and subheader with custom styles
-st.markdown('<p class="title">Snack-O-Meter</p>', unsafe_allow_html=True)
-st.markdown(
-    """
-    <p class="subheader">Scan, Snack, Stay Healthy!</p>
-    <p class="subheader">Scan your snack, uncover its nutrients, and receive instant health insights! Make mindful snacking a breeze</p>
-    """,
-    unsafe_allow_html=True
-)
+st.title("Snack-O-Meter")
+st.subheader("Scan, Snack, Stay Healthy! Scan your snack, uncover its nutrients, and receive instant health insights! Make mindful snacking a breeze")
+
 
 st.divider()
 
