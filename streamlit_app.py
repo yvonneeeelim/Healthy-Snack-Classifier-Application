6f281caa6fa8d967356e3a897b9abdffcb8b305e
yvonneeeelim <141410050+yvonneeeelim@users.noreply.github.com>
2023-10-12 14:50:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,10 +54,10 @@ with tab1:
    st.header("Enter Your Nutrients")
     
    # Get user input for nutrients
-   sugar = float(st.number_input("Enter Sugar (g):", format="%.3f", value=1))
-   fats = float(st.number_input("Enter Fats (g):", format="%.3f", value=1))
-   sodium = float(st.number_input("Enter Sodium (g):", format="%.3f", value=1))
-   serving_size = float(st.number_input("Enter Serving Size (g):", value=1))
+   sugar = float(st.number_input("Enter Sugar (g):", format="%.3f", value=1.000))
+   fats = float(st.number_input("Enter Fats (g):", format="%.3f", value=1.000))
+   sodium = float(st.number_input("Enter Sodium (g):", format="%.3f", value=1.000))
+   serving_size = float(st.number_input("Enter Serving Size (g):", value=1.000))
 
    # calculate the nutrition value for prediction
    fats_per_gram = fats/serving_size
